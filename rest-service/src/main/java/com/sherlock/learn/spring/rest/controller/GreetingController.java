6b63b4a9976ccb9b11a2cc082c5f2731930b70ec
Sandeep L <sandeep.lakdawala@gmail.com>
2019-01-20 15:39:46 +0530

    Rest-Service-Contains Sleuth+Zipkin integration
    Signed-off-by: Sandeep L <sandeep.lakdawala@gmail.com>

---
+++ rest-service/src/main/java/com/sherlock/learn/spring/rest/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.sherlock.learn.spring.rest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello ";
+    private static Logger logger = LoggerFactory.getLogger(GreetingController.class);
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.info("Greeting :" + name);
+        return String.format(template + name);
+    }
+}
